4f0dca7471f85e4007cf10e9f28193bad13c929a
Mahad <mahad.abd2003@gmail.com>
2025-09-04 20:15:46 +0400

    feet: BLESSED by Yousaf's BRAIN AGAIN

--- ex01/RPN.cpp
+++ ex01/RPN.cpp
@@ -46,15 +46,9 @@ inline std::string& trim(std::string& s)
  * @return int 0 if successful or -1 otherwise
  */
 int    RPN::_parse(const std::string& charToAdd) {
-    if (charToAdd.length() > 2) {
-        return (-1);
-    }
-    if (charToAdd.length() == 2 && (charToAdd[0] != '+' && charToAdd[0] != '-')) {
-        return (-1);
-    }
-    if (charToAdd.find_first_of(".()") != std::string::npos)
-        return (-1);
     if (charToAdd.find_first_not_of("1234567890+-*/") != std::string::npos)
+        return (-1);
+    if (charToAdd.length() > 2 || (charToAdd.length() == 2 && (charToAdd[0] != '+' && charToAdd[0] != '-')))
         return (-1);
     return (0);
 }
